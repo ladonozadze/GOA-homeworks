ef2504b1bb8a04c5fd6f35870ad86cecd15c9c60
ladonozadze <ladonozadze213@gmail.com>
2024-02-28 19:54:31 +0400

    commit

--- Day 7/aritmetika.py
+++ Day 7/aritmetika.py
@@ -1,28 +1,28 @@
-number1=input("enter number(1): " )
-number2=input("enter number(2): " )
+number1=int(input("enter number(1): " ))
+number2=int(input("enter number(2): " ))
 
 
 print("number1 type", type(number1))  
 
 print("number1 type", type (number2))
 
-print("Addition:", int(number1) +int(number2))
+print("Addition:", (number1) +(number2))
 
-print("Subtraction:", int(number1)-int(number2))
+print("Subtraction:", (number1)-(number2))
 
-print("multiplication:", int(number1)*int(number2))
+print("multiplication:", (number1)*(number2))
 
-print("division:", int(number1)/int(number2))
+print("division:", (number1)/(number2))
 
-print("whole number:", int(number1) // int(number2))
+print("whole number:", (number1) // (number2))
 
-print("residual:", int(number1)%int(number2) )
+print("residual:", (number1)%(number2) )
 
-print("number1>number2:", int(number1)>int(number2) )
+print("number1>number2:", (number1)>(number2) )
 
-print("number1<number2:", int(number1)<int(number2) )
+print("number1<number2:", (number1)<(number2) )
 
-print("number1=number2:", int(number1)==int(number2) )
+print("number1=number2:", (number1)==(number2) )
 
 
 
