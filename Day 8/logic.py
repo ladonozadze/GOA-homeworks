d351db236b3483092759ad72a88208445bbaf26e
ladonozadze <ladonozadze213@gmail.com>
2024-02-28 22:29:21 +0400

    commit

---
+++ Day 8/logic.py
@@ -0,0 +1,13 @@
+required_pushups = 100
+required_squats=50
+
+pushup =int(input("how many pushups have you done? :"))
+squats=int(input("how many squats have you done? :"))
+
+print(pushup == required_pushups and squats==required_squats)
+
+if pushup == required_pushups and squats==required_squats==True :
+    print("congratulations Broo!")
+
+if pushup == required_pushups and squats==required_squats==False :
+    print("you failed broo!")
