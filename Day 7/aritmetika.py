0a5d937cbe95fd112c18c2cea770690234cfd79b
ladonozadze <ladonozadze213@gmail.com>
2024-02-28 19:35:19 +0400

    commit

---
+++ Day 7/aritmetika.py
@@ -0,0 +1,34 @@
+number1=input("enter number(1): " )
+number2=input("enter number(2): " )
+
+
+print("number1 type", type(number1))  
+
+print("number1 type", type (number2))
+
+print("Addition:", int(number1) +int(number2))
+
+print("Subtraction:", int(number1)-int(number2))
+
+print("multiplication:", int(number1)*int(number2))
+
+print("division:", int(number1)/int(number2))
+
+print("whole number:", int(number1) // int(number2))
+
+print("residual:", int(number1)%int(number2) )
+
+print("number1>number2:", int(number1)>int(number2) )
+
+print("number1<number2:", int(number1)<int(number2) )
+
+print("number1=number2:", int(number1)==int(number2) )
+
+
+
+
+
+
+  
+
+
