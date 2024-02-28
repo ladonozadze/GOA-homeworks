5caec05064a98ce226fca4fb4b71c7ffef663b05
ladonozadze <ladonozadze213@gmail.com>
2024-02-28 22:32:46 +0400

    commit

---
+++ Day 8/logic2.py
@@ -0,0 +1,8 @@
+required_pushups =150
+required_squats=200
+
+pushup =int(input("how many pushups have you done? :"))
+squats=int(input("how many squats have you done? :"))
+
+x=print(pushup == required_pushups or squats==required_squats)
+
